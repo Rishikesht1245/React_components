a895087eabb0729bd14b2186178eec65ea8f0a6b
Rishikesh T <rishikeshtgit@gmail.com>
2023-10-01 16:19:25 +0530

    Implemented : useMemo, React.memo, and useCall back hooks

--- 9_Supense_useMemo_memo_useCallback/src/memo/MemoChild.jsx
+++ 9_Supense_useMemo_memo_useCallback/src/memo/MemoChild.jsx
@@ -3,14 +3,15 @@ import { memo, useCallback, useMemo } from "react";
 const MemoChild = ({ count }) => {
   const print = () => {
     console.log(count);
+    return count + 1;
   };
 
   const counter = useMemo(() => print(), []);
 
-  counter();
   return (
     <div className="flex flex-col ">
       <h1>This is the Memo Child </h1>
+      <h1>Counter : {counter}</h1>
     </div>
   );
 };
