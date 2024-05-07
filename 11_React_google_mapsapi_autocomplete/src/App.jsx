182851e9253d9134ade84197685ccc02d2b09c99
Rishikesh T <rishikeshtgit@gmail.com>
2024-05-07 22:34:40 +0530

    Update : implemented the UI

--- 11_React_google_mapsapi_autocomplete/src/App.jsx
+++ 11_React_google_mapsapi_autocomplete/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Map from "./components/Map";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="bg-[#0e1123] py-10 min-h-[100vh]">
+      <div className="flex flex-col border border-gray-600 shadow-lg rounded-sm justify-center items-center h-full max-w-[800px] w-full mx-auto p-5">
+        {/* Inputs for coordinates and location */}
+        <div className="flex flex-row items-center w-full justify-center gap-10">
+          <div className="flex flex-col items-start gap-1 ">
+            <label
+              htmlFor="coordinates"
+              className="text-[16px] text-white font-semibold ms-1"
+            >
+              Coordinates :
+            </label>
+            <input
+              id="coordinates"
+              type="text"
+              className="px-4 py-2 border focus:outline-none rounded-sm shadow-sm"
+              placeholder="latitude, longitude"
+              name="coordinates"
+            />
+          </div>
+          <div className="flex flex-col items-start gap-1">
+            <label
+              htmlFor="location"
+              className="text-white text-[16px] font-semibold ms-1"
+            >
+              Location :
+            </label>
+            <input
+              id="location"
+              type="text"
+              className="px-4 py-2 border focus:outline-none rounded-sm shadow-sm"
+              name="location"
+            />
+          </div>
+        </div>
+
+        {/* Map component comes here */}
+        <Map coordinates={""} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
