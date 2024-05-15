0abc2ec21c31c030be70e6bbf0ba3dda7b01d7f9
Rishikesh T <rishikeshtgit@gmail.com>
2024-05-16 00:18:29 +0530

    Created Map

--- 11_React_google_mapsapi_autocomplete/src/App.jsx
+++ 11_React_google_mapsapi_autocomplete/src/App.jsx
@@ -1,27 +1,49 @@
 import Map from "./components/Map";
+import {
+  Autocomplete,
+  LoadScript,
+  useJsApiLoader,
+} from "@react-google-maps/api";
 
 function App() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    libraries: ["places", "geocoding"],
+  });
+
+  if (loadError) {
+    return <div>Error loading Google Maps API</div>;
+  }
+
   return (
-    <main className="bg-[#0e1123] py-10 min-h-[100vh]">
-      <div className="flex flex-col border border-gray-600 shadow-lg rounded-sm justify-center items-center h-full max-w-[800px] w-full mx-auto p-5">
+    <main className="bg-[#0e1123] py-4 min-h-[100vh]">
+      <h3 className="text-white font-black text-xl text-center mb-2">
+        The Map AutoComplete
+      </h3>
+      <div className="flex flex-col gap-6 border border-gray-600 shadow-lg rounded-sm justify-center items-center h-[90vh] max-w-[800px] lg:max-w-[1024px] w-full mx-auto p-5">
         {/* Inputs for coordinates and location */}
         <div className="flex flex-row items-center w-full justify-center gap-10">
-          <div className="flex flex-col items-start gap-1 ">
+          <div className="flex flex-col flex-1 items-start gap-1 w-full">
             <label
               htmlFor="coordinates"
               className="text-[16px] text-white font-semibold ms-1"
             >
               Coordinates :
             </label>
-            <input
-              id="coordinates"
-              type="text"
-              className="px-4 py-2 border focus:outline-none rounded-sm shadow-sm"
-              placeholder="latitude, longitude"
-              name="coordinates"
-            />
+            {isLoaded && (
+              <Autocomplete>
+                <input
+                  id="coordinates"
+                  type="text"
+                  className="px-4 py-2 border focus:outline-none rounded-sm shadow-sm w-full"
+                  placeholder="latitude, longitude"
+                  name="coordinates"
+                />
+              </Autocomplete>
+            )}
           </div>
-          <div className="flex flex-col items-start gap-1">
+          <div className="flex flex-1 flex-col items-start gap-1 w-full">
             <label
               htmlFor="location"
               className="text-white text-[16px] font-semibold ms-1"
@@ -31,14 +53,14 @@ function App() {
             <input
               id="location"
               type="text"
-              className="px-4 py-2 border focus:outline-none rounded-sm shadow-sm"
+              className="px-4 py-2 border focus:outline-none rounded-sm shadow-sm w-full"
               name="location"
             />
           </div>
         </div>
 
         {/* Map component comes here */}
-        <Map coordinates={""} />
+        <Map isLoaded={isLoaded} />
       </div>
     </main>
   );
--- 11_React_google_mapsapi_autocomplete/src/components/Map.jsx
+++ 11_React_google_mapsapi_autocomplete/src/components/Map.jsx
@@ -1,4 +1,41 @@
-const Map = () => {
-  return <div>Map</div>;
+import React from "react";
+import { GoogleMap } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
 };
-export default Map;
+
+const center = {
+  lat: 20.745,
+  lng: 78.523,
+};
+
+function Map({ isLoaded }) {
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={5}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(Map);
