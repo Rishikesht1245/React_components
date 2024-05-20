5eafcfe455c047d24c6c70cfc95db18a18f76aeb
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-05-20 16:23:18 +0530

    update : implemented the pdf downloading functionality.

--- 12_Save_as_PDF/src/App.jsx
+++ 12_Save_as_PDF/src/App.jsx
@@ -1,19 +1,26 @@
-import { Button, Container } from "react-bootstrap"
-import Pdf from "./components/Pdf"
+import { Button, Container } from "react-bootstrap";
+import Pdf from "./components/Pdf";
 import { FaDownload } from "react-icons/fa";
+import { usePDF } from "react-to-pdf";
 
 function App() {
+  const { toPDF, targetRef } = usePDF({ filename: "invoice.pdf" });
 
   return (
     <>
       <Container className="d-flex flex-column w-100 py-5 my-5 border">
-        <Pdf/>
+        <div ref={targetRef} className="w-full h-full flex items-center justify-center p-10">
+          <Pdf />
+        </div>
+
         <div className="mt-5 mx-auto">
-          <Button variant="danger">Save as PDF <FaDownload/></Button>
+          <Button variant="danger" onClick={() => toPDF()}>
+            Save as PDF <FaDownload />
+          </Button>
         </div>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
