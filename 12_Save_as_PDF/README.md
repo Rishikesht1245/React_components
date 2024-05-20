0369abd725e507da6b676160357e40d698e264e2
Rishikesht1245 <rishikeshtgit@gmail.com>
2024-05-20 16:32:41 +0530

    Update : implemented csv download functionality using react-json-to-csv package.

--- 12_Save_as_PDF/README.md
+++ 12_Save_as_PDF/README.md
@@ -1,3 +1,6 @@
-# REACT TO PDF 
+# REACT TO PDF AND CSV 
 
+- Used Bootstrap 5 and Vite for bootstrapping the project
 - This component is using react-to-pdf package for creating pdf s from react components.
+- Also it uses react-json-to-csv package for downloading the data in csv (comma separated value) format.
+
--- 12_Save_as_PDF/src/App.jsx
+++ 12_Save_as_PDF/src/App.jsx
@@ -1,22 +1,44 @@
 import { Button, Container } from "react-bootstrap";
 import Pdf from "./components/Pdf";
 import { FaDownload } from "react-icons/fa";
+import { FaFileCsv } from "react-icons/fa";
 import { usePDF } from "react-to-pdf";
+import CsvDownloadButton from 'react-json-to-csv'
 
 function App() {
   const { toPDF, targetRef } = usePDF({ filename: "invoice.pdf" });
 
+  const csvData = [
+    {
+      Amount: "$100000",
+      Place: "United Arab Emirates",
+      "Last Date": "19-May-2024",
+    },
+    {
+      Amount: "$100000",
+      Place: "United Arab Emirates",
+      "Last Date": "19-May-2024",
+    },
+  ];
+
   return (
     <>
       <Container className="d-flex flex-column w-100 py-5 my-5 border">
-        <div ref={targetRef} className="w-full h-full flex items-center justify-center p-10">
+        <div
+          ref={targetRef}
+          className="w-full h-full flex items-center justify-center p-10"
+        >
           <Pdf />
         </div>
 
-        <div className="mt-5 mx-auto">
+        <div className="mt-5 d-flex align-items-center justify-content-center gap-5 w-full">
           <Button variant="danger" onClick={() => toPDF()}>
             Save as PDF <FaDownload />
           </Button>
+          <CsvDownloadButton 
+          className="btn btn-success" data={csvData} delimiter="," filename="invoice_report">
+            Save as CSV <FaFileCsv />
+          </CsvDownloadButton>
         </div>
       </Container>
     </>
